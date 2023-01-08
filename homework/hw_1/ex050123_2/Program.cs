42d1c3e6bc5152e2d7c9ec1f584a72a00acb8366
Ливерова Александра <aleax57.89@mail.ru>
2023-01-08 21:35:16 +0300

    05.01.2023

---
+++ homework/hw_1/ex050123_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("max=" + max);
---
+++ homework/hw_1/ex050123_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while (index <= n)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+    index = index + 1;
+}
