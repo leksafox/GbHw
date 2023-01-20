89d2aa011f3fc4138a0213df0d78d15849b65b51
Ливерова Александра <aleax57.89@mail.ru>
2023-01-21 02:40:14 +0300

    добавила дз за 20 января

---
+++ homework/hw_9/ex200123_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+// Метод нахождения суммы натуральных элементов в промежутке от M до N
+int FindSum(int numberM, int numberN)
+{
+    if (numberM <= numberN)
+    {
+        return numberM + FindSum(numberM + 1, numberN);
+    }
+    else
+        return 0;
+}
+
+Console.WriteLine($"Сумма натуральных элементов от {numberM} до {numberN} равна {FindSum(numberM, numberN)}");
---
+++ homework/hw_9/ex200123_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите неотрицательное число M:");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите неотрицательное число N:");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+//Метод вычисления функции Аккермана:
+int AckerFunction(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+    else
+    if (numM > 0 && numN == 0)
+        return AckerFunction(numM - 1, 1);
+    return AckerFunction(numM - 1, AckerFunction(numM, numN - 1));
+}
+
+Console.WriteLine($"M = {numM}, N = {numN} -> A(m,n) = {AckerFunction(numM, numN)}");
